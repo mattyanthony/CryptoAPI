854fda54d800d61596b979fe4ad4387ca158ab3d
mattyanthony <108240399+mattyanthony@users.noreply.github.com>
2024-03-11 21:24:34 -0400

    hit the API, got a list of all the coins displayed

--- cryptoAPI/src/App.jsx
+++ cryptoAPI/src/App.jsx
@@ -1,13 +1,45 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [list, setList] = useState(null);
 
+  useEffect(() => {
+    const fetchAllCoinData = async () => {
+      try {
+        const response = await fetch('https://min-api.cryptocompare.com/data/all/coinlist?&api_key', {
+          headers: {
+            'Authorization': `Apikey ${API_KEY}`
+          }
+        });
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setList(data);
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+      }
+    };
 
+    fetchAllCoinData().catch(console.error);
+  }, []);
+
+  return (
+    <div className="whole-page">
+      <h1>My Crypto List</h1>
+      <ul>
+        {list && Object.entries(list.Data).map(([key, value]) => 
+          value.PlatformType === "blockchain" ? (
+            <li key={key}>{value.FullName}</li>
+          ) : null
+        )}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
