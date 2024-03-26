6fb9a818228d2a37942ce3b152f5384f260ea85c
mattyanthony <108240399+mattyanthony@users.noreply.github.com>
2024-03-25 21:36:20 -0400

    created a homebutton (added layout.jsx, updated main.jsx, added some css)

---
+++ cryptoAPI/src/Routes/Layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li className="home-link">
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Layout;
--- cryptoAPI/src/main.jsx
+++ cryptoAPI/src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App'; // Adjust the import path as necessary
+import Layout from './Routes/Layout'; // Adjust the import path as necessary
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index={true} element={<App />} />
+          {/* Add other routes here */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
